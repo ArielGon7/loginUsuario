2cc849d96a28e53a69bbe8d59c6041f6ee60acbe
ArielGon7 <arielgomessantos7@gmail.com>
2026-02-02 23:33:49 -0300

    Implementação da anotação @Column no Entity

--- src/main/java/com/devari/loginUsuario/entity/Usuario.java
+++ src/main/java/com/devari/loginUsuario/entity/Usuario.java
@@ -1,13 +1,11 @@
 package com.devari.loginUsuario.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.repository.query.Param;
 
 @Getter
 @Setter
@@ -19,7 +17,11 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name", unique = true, nullable = false)
     private String name;
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
 
 }
