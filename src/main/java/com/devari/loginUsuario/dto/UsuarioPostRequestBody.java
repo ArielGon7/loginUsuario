fc9dcc88205a2b101ca8753bc1887dce50947559
ArielGon7 <arielgomessantos7@gmail.com>
2026-02-04 19:27:49 -0300

    Usuario Post da requisição para DTO criado.

---
+++ src/main/java/com/devari/loginUsuario/dto/UsuarioPostRequestBody.java
@@ -0,0 +1,16 @@
+package com.devari.loginUsuario.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class UsuarioPostRequestBody {
+    @JsonProperty(required = true)
+    private String name;
+    @JsonProperty(required = true)
+    private String email;
+    @JsonProperty(required = true)
+    private String password;
+
+}
