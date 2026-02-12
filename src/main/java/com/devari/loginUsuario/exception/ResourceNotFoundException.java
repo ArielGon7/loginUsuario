3f375141e183519dd76df52d60e2aaa0a04f2a0e
ArielGon7 <arielgomessantos7@gmail.com>
2026-02-12 12:03:46 -0300

    Criação da classe ResourceNotFoundException.java

---
+++ src/main/java/com/devari/loginUsuario/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.devari.loginUsuario.exception;
+
+public class ResourceNotFoundException extends Exception{
+
+    ResourceNotFoundException(String message){
+        super(message);
+    }
+}
